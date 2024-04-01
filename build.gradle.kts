a3d8d0be8040788c4d76021df4b0e8d0d6f03c38
Tyonakaisan <mikanno82@gmail.com>
2024-04-01 19:47:15 +0900

    Version up

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ paper {
     val mainPackage = "github.tyonakaisan.horsechecker"
     generateLibrariesJson = true
     name = rootProject.name
-    version = "1.5.0-SNAPSHOT"
+    version = "1.5.1-SNAPSHOT"
     main = "$mainPackage.HorseChecker"
     loader = "$mainPackage.HorseCheckerLoader"
     bootstrapper = "$mainPackage.HorseCheckerBootstrap"
